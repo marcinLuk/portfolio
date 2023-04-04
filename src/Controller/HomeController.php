947336370be6880c4129b75581fe041d8ad5763a
marcin.lukaszek@appwise.pl <marcin.lukaszek@appwise.pl>
2023-04-04 14:43:50 +0200

    added vue

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Project;
 use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,8 +12,6 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index( ProjectRepository $projectRepository ): Response
     {
-        return $this->render('home/index.html.twig', [
-            'data' => 'HomeController',
-        ]);
+        return new Response('Home');
     }
 }
